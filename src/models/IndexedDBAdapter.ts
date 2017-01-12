bbe12bfc8d207aebc59f6b66095e29039075484a
yajamon <yajamon.tatsuki@gmail.com>
2017-01-12 23:22:47 +0900

    Add IndexdedDBAdapter class

---
+++ src/models/IndexedDBAdapter.ts
@@ -0,0 +1,30 @@
+import DBConfig from "../interfaces/IndexedDBConfig";
+/**
+ * IndexedDBAdapter
+ */
+export default class IndexedDBAdapter {
+    private db: IDBDatabase;
+    constructor() {
+    }
+
+    open(config:DBConfig){
+        let request = indexedDB.open(config.name, config.version);
+        request.onsuccess = (event)=>{
+            if(!(event.target instanceof IDBOpenDBRequest)){
+                return ;
+            }
+            if(!(event.target.result instanceof IDBDatabase)){
+                return ;
+            }
+            console.log("success open database:"+config.name);
+            this.db = event.target.result;
+        };
+        request.onupgradeneeded = (event)=>{
+            if(!(event instanceof IDBVersionChangeEvent && event.target instanceof IDBOpenDBRequest && event.target.result instanceof IDBDatabase)){
+                return ;
+            }
+            console.log("need Upgrade! old:" + event.oldVersion + " new:"+event.newVersion);
+            this.db = event.target.result;
+        }
+    }
+}
