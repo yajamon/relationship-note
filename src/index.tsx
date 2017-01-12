16deebaf029ae4125fb64b314646c02d6c9aa2cd
yajamon <yajamon.tatsuki@gmail.com>
2017-01-12 23:23:12 +0900

    Use IndexedDBAdapter class

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,11 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { Hello } from "./components/Hello";
+import Config from "./configs/DataBase";
+import IndexedDBAdapter from "./models/IndexedDBAdapter";
+
+const dbAdapter = new IndexedDBAdapter();
+dbAdapter.open(Config);
 
 ReactDOM.render(
     <Hello compiler="TypeScript" framework="React" />,
