30d3cd4783eb01f897fb8bd06db778399494596b
yajamon <yajamon.tatsuki@gmail.com>
2017-01-16 00:56:33 +0900

    Use App component

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,9 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { Hello } from "./components/Hello";
-import Config from "./configs/DataBase";
-import Migrator from "./migration/Migrator"
-import IndexedDBAdapter from "./models/IndexedDBAdapter";
-
-const dbAdapter = new IndexedDBAdapter();
-dbAdapter.open(Config, new Migrator());
+import { App } from "./components/App";
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
+    <App />,
     document.getElementById("mainApp")
 );
