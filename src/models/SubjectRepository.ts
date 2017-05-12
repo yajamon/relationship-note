746e3d0e93451a85b3a8a02eac3d02e5055e1372
yajamon <yajamon.tatsuki@gmail.com>
2017-05-12 12:36:02 +0900

    Add storename property

--- src/models/SubjectRepository.ts
+++ src/models/SubjectRepository.ts
@@ -1,10 +1,11 @@
 import IndexedDBAdapter from "./IndexedDBAdapter";
 
 export default class SubjectRepository {
+    readonly storeName:string = "subject";
     constructor(private adapter:IndexedDBAdapter) {
     }
 
     findAll(){
-        return this.adapter.findAll("subject");
+        return this.adapter.findAll(this.storeName);
     }
 }
