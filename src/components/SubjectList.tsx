104fe25b5bcbecee5f6849a39ae2fad1fe4626fd
yajamon <yajamon.tatsuki@gmail.com>
2017-05-22 00:09:28 +0900

    Change format of Component: SubjectList

--- src/components/SubjectList.tsx
+++ src/components/SubjectList.tsx
@@ -6,13 +6,15 @@ export interface SubjectProps {
     subjects: SubjectRecord[];
 }
 
-export function SubjectList(props: SubjectProps) {
-    const subjects = props.subjects.map((subject) => {
+export class SubjectList extends React.Component<SubjectProps, undefined> {
+    render() {
+        const subjects = this.props.subjects.map((subject) => {
+            return (
+                <li key={subject.id} >{subject.name}</li>
+            );
+        });
         return (
-            <li key={subject.id} >{subject.name}</li>
+            <ol>{subjects}</ol>
         );
-    });
-    return (
-        <ol>{subjects}</ol>
-    );
+    }
 }
