c281d14b3bfcb842e21c5182a7433dba6853b9cd
yajamon <yajamon.tatsuki@gmail.com>
2017-05-22 23:44:03 +0900

    Remove unused import and state parameter

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,20 +5,16 @@ import Migrator from "../migration/Migrator"
 import IDBConnector from "../infrastructure/IndexedDBConnector"
 import IndexedDBAdapter from "../models/IndexedDBAdapter";
 
-import SubjectRecord from "../interfaces/SubjectRecord";
 import {SubjectList} from "../components/SubjectList";
 import {AddSubject} from "../components/AddSubject";
-import SubjectRepository from "../models/SubjectRepository"
 
 interface AppState {
-    subjects: SubjectRecord[];
 }
 
 export class App extends React.Component<undefined, AppState> {
     constructor() {
         super();
         this.state = {
-            subjects: []
         };
         IDBConnector.open(Config, new Migrator())
             .then(() => {
