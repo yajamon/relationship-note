eef755241a986fb0683833ef6459d67458457f67
yajamon <yajamon.tatsuki@gmail.com>
2017-08-04 20:49:32 +0900

    Add component: TagList

---
+++ src/components/TagList.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+
+import IndexedDBAdapter from "../models/IndexedDBAdapter";
+import TagRepository from "../models/TagRepository"
+
+import TagRecord from "../interfaces/TagRecord";
+
+import {Observer} from "../interfaces/Observer"
+import observable from "../infrastructure/IndexedDBStoreObservable"
+
+
+interface TagListProps {
+}
+
+interface TagListState {
+    tags: TagRecord[];
+}
+
+export class TagList extends React.Component<TagListProps, TagListState> {
+    constructor(){
+        super();
+        this.state = {
+            tags: [],
+        }
+    }
+
+
+    componentWillMount(){
+        console.log("will mount");
+        this.pullSubjects();
+        observable.instance.addObserverToStore("tag", this);
+    }
+    componentWillUnmount(){
+        console.log("will unmount");
+        observable.instance.removeObserverByStore("tag", this);
+    }
+
+    pullSubjects(){
+        const repo = new TagRepository(new IndexedDBAdapter());
+        repo.findAll().then((tags: TagRecord[]) => {
+            console.log('complete read subjects.');
+            this.setState({
+                tags: tags
+            });
+        });
+    }
+
+    update() {
+        this.pullSubjects();
+    }
+
+    render() {
+        const tags = this.state.tags.map((tag) => {
+            return (
+                <li key={tag.id} >{tag.name}</li>
+            );
+        });
+        return (
+            <ol>{tags}</ol>
+        );
+    }
+}
