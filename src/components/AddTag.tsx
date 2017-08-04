f13c30ffc69b19341d7a4a516a9058dc916e521a
yajamon <yajamon.tatsuki@gmail.com>
2017-08-04 20:38:22 +0900

    Add component: AddTag

---
+++ src/components/AddTag.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+interface AddTagProps {
+}
+
+interface AddTagState {
+}
+
+export class AddTag extends React.Component<AddTagProps, AddTagState> {
+    constructor(){
+        super();
+        this.state = {
+        }
+    }
+
+    render(){
+        return (
+            <div></div>
+        );
+    }
+}
