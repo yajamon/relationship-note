8b897f957496fa90cf3748539447361f176f7b8f
yajamon <yajamon.tatsuki@gmail.com>
2018-03-28 03:39:09 +0900

    RED: 文字列生成のFake実装潰し

--- src/domain/name.test.ts
+++ src/domain/name.test.ts
@@ -1,6 +1,9 @@
 import { Name } from './name';
 
 describe('Name', () => {
+    it('文字列を引数に生成できる', () => {
+        let _ = new Name('Taro');
+    });
     it('空文字では生成できない', () => {
         expect(() => {
             let _ = new Name('');
