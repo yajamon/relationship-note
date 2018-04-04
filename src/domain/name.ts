2a1b03be535ac783c78e66d7592cec615585262d
yajamon <yajamon.tatsuki@gmail.com>
2018-04-04 20:08:22 +0900

    GREEN: Name.value はコンストラクタ引数の値と同じ

--- src/domain/name.ts
+++ src/domain/name.ts
@@ -2,6 +2,6 @@ export class Name {
     readonly value: string;
     constructor(value: string) {
         if (value === '') { throw Error(); }
-        this.value = 'Taro';
+        this.value = value;
     }
 }
