d58c5a3b5381104f75a9118114d33b94204b09fa
yajamon <yajamon.tatsuki@gmail.com>
2018-04-24 18:04:48 +0900

    RED: NumberThingIdは1以上の数で生成できる

--- src/infrastructure/number_thing_id.test.ts
+++ src/infrastructure/number_thing_id.test.ts
@@ -1,6 +1,9 @@
 import { NumberThingId } from './number_thing_id';
 
 describe('NumberThingId', () => {
+    it('1以上の数で生成できる', () => {
+        expect(new NumberThingId(1)).not.toBeNull();
+    });
     it('0以下の数字では生成できない', () => {
         expect(() => {
             return new NumberThingId(0);
