aa6155054ba12a37640a9e586f29802bad7b0fb9
yajamon <yajamon.tatsuki@gmail.com>
2018-04-26 16:26:43 +0900

    RED: incrementalGenerator next()を呼ぶ度+1

--- src/infrastructure/incremental_generator.test.ts
+++ src/infrastructure/incremental_generator.test.ts
@@ -9,4 +9,11 @@ describe('IncrementalGenerator', () => {
         let gen2 = incrementalGenerator(second);
         expect(gen2.next().value).toBe(second);
     });
+    it('next()を呼ぶ度、前回の値+1 の値を返す', () => {
+        let first = 5;
+        let gen = incrementalGenerator(first);
+        expect(gen.next().value).toBe(first);
+        expect(gen.next().value).toBe(first + 1);
+        expect(gen.next().value).toBe(first + 2);
+    });
 });
