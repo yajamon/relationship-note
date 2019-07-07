8bc7d98675134379eeea6e93909db2291188fc17
yajamon <yajamon.tatsuki@gmail.com>
2019-07-07 19:30:33 +0900

    ThingFactoryを作成

---
+++ src/domain/thing_factory.ts
@@ -0,0 +1,13 @@
+import { Thing } from "./thing";
+import { ThingRepository } from "./thing_repository";
+import { Name } from "./name";
+
+export class ThingFactory {
+  constructor(private repository: ThingRepository) {
+  }
+  public create(thingName: Name): Thing {
+    const id = this.repository.nextIdentifier();
+    const thing = new Thing(id, thingName);
+    return thing;
+  }
+}
