c67ae3395ee17fe747de4380b4339ff41a1a52f7
yajamon <yajamon.tatsuki@gmail.com>
2020-02-09 17:42:26 +0900

    Nameの生成方法変更に伴いテストを修正

--- src/infrastructure/map_thing_repository.test.ts
+++ src/infrastructure/map_thing_repository.test.ts
@@ -1,6 +1,7 @@
-import { MapThingRepository } from './map_thing_repository';
-import { Thing } from '../domain/thing';
+import { unwrap } from "@yajamon/result.ts";
 import { Name } from '../domain/name';
+import { Thing } from '../domain/thing';
+import { MapThingRepository } from './map_thing_repository';
 
 describe('MapThingRepository', () => {
   describe('nextIdentifier()', () => {
@@ -16,14 +17,14 @@ describe('MapThingRepository', () => {
     it('保存したThingをThingIdで取り出すことができる', () => {
       let repo = new MapThingRepository();
       let id = repo.nextIdentifier();
-      let name = new Name('Taro');
+      let name = unwrap(Name.create('Taro'));
       let thing = new Thing(id, name);
       repo.save(thing);
       let result = repo.findByThingId(id);
       expect(result).toBe(thing);
 
       // 別のデータが追加されても適切にThingが取得できる
-      repo.save(new Thing(repo.nextIdentifier(), new Name('dust')));
+      repo.save(new Thing(repo.nextIdentifier(), unwrap(Name.create('dust'))));
       let result2 = repo.findByThingId(id);
       expect(result2).toBe(thing);
     });
